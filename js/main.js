c7b5922b7dcd964e1cf92438959ba78b3022440f
Robert-Kiprotich <kyprohz@gmail.com>
2021-02-05 14:47:47 +0300

    frontend first  commit

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+    $('.color-choose input').on('click', function () {
+        var headphonesColor = $(this).attr('data-image');
+        $('.active').removeClass('active');
+        $('.left-column img[data-image=' + headphonesColor + ']').addClass('active');
+        $(this).addClass('active');
+
+
+    });
+});
+$(document).ready(function () {
+    $('.slider').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
+
+//navigation*/
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+
+
+
+
